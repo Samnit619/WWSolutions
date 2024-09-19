8b9fac6e54a7a7654e909a7851d1067a61623e5c
Samnit619 <samnit.s472@gmail.com>
2024-09-19 14:40:06 -0400

    created directories

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className=" bg-[#333333] h-screen flex justify-center items-center">
-      <Text className="text-[#a3a3a3]">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View className=" bg-[#333333] h-screen flex  items-center">
+      <Text className="text-[#a3a3a3] text-2xl">WorldWirelessSolutions</Text>
     </View>
   );
 }
