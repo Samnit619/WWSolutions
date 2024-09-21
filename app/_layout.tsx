f0603c92f42c03ca701785a3be641520adfee4f3
Samnit619 <samnit.s472@gmail.com>
2024-09-20 21:04:12 -0400

    started signin page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="auth" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, statusBarColor: "black" }}
+      />
+      <Stack.Screen name="auth" options={{ statusBarColor: "black" }} />
     </Stack>
   );
 }
--- app/auth/Signin.tsx
+++ app/auth/Signin.tsx
@@ -1,11 +1,20 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Pressable, Image } from "react-native";
 import React from "react";
 
 const Signin = () => {
   return (
-    <View className=" h-screen bg-[#333333]">
-      <Button title="Log in with Googgle"></Button>
-      <Text className=" text-blue-600">Hello</Text>
+    //
+    <View className=" h-screen px-3">
+      <Pressable className="bg-blue-500 rounded-full px-4 py-3.5 mt-6 flex-row items-center">
+        <Image
+          source={require("../../assets/images/google.png")}
+          className="h-[20px] w-[20px] bg-blue-500 rounded-full"
+        />
+        <Text className="text-white font-semibold ml-[95px]">
+          Log in with Google
+        </Text>
+      </Pressable>
+      <View className=" border-b border-b-[#cecece] mt-5"></View>
     </View>
   );
 };
