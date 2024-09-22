c33acab9da8c9d464cd91ebfa737a8876ac83e9e
Samnit619 <samnit.s472@gmail.com>
2024-09-22 12:00:11 -0400

    changes in sign in page

--- app/auth/Signin.tsx
+++ app/auth/Signin.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, Pressable, Image } from "react-native";
+import { View, Text, Button, Pressable, Image, TextInput } from "react-native";
 import React from "react";
 
 const Signin = () => {
@@ -8,13 +8,23 @@ const Signin = () => {
       <Pressable className="bg-blue-500 rounded-full px-4 py-3.5 mt-6 flex-row items-center">
         <Image
           source={require("../../assets/images/google.png")}
-          className="h-[20px] w-[20px] bg-blue-500 rounded-full"
+          className="h-[20px] w-[20px] rounded-full"
         />
         <Text className="text-white font-semibold ml-[95px]">
           Log in with Google
         </Text>
       </Pressable>
+      <Pressable className="bg-[#3b5998] rounded-full px-4 py-3.5 mt-3 flex-row items-center">
+        <Image
+          source={require("../../assets/images/facebook.png")}
+          className="h-[20px] w-[20px] rounded-full"
+        />
+        <Text className="text-white font-semibold ml-[95px]">
+          Log in with Facebook
+        </Text>
+      </Pressable>
       <View className=" border-b border-b-[#cecece] mt-5"></View>
+      <TextInput />
     </View>
   );
 };
