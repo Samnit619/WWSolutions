0d700810c5c9c41c18e57ca3faa20bf75cef0e40
Samnit619 <samnit.s472@gmail.com>
2024-11-08 10:00:48 -0500

    added profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>asflkjalsfjlakjs</Text>
+    <View className="bg-white h-screen p-5">
+      <View className="flex-col items-center justify-center mt-4">
+        <Image
+          source={require("../../assets/images/samnit123.jpg")}
+          className="h-[120px] w-[120px] rounded-full mt-5 mb-3"
+        />
+        <Text className="text-3xl font-medium text-[#3e3e3e] p-1">
+          Samnit Samnit
+        </Text>
+        <Text className="text-[#565656]">Lost in the right direction</Text>
+      </View>
     </View>
   );
 };
