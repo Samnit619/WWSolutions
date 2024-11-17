223d339ee2a443829e92f38d120689e51b20c483
Samnit619 <samnit.s472@gmail.com>
2024-11-16 23:38:27 -0500

    added gradient

--- app/(pages)/aboutUs.tsx
+++ app/(pages)/aboutUs.tsx
@@ -2,32 +2,42 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import { Button, NativeBaseProvider } from "native-base";
 import { useNavigation } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import { ScrollView } from "native-base";
 
 const AboutUs = () => {
   const navigation = useNavigation();
 
   return (
     <NativeBaseProvider>
-      <View className="h-screen p-4 bg-white">
-        <Button
-          className="rounded-full w-7 bg-transparent "
-          onPress={() => navigation.goBack()}
-        >
-          <Image
-            source={require("../../assets/images/ItSolutions/arrow.png")}
-            className="w-7 h-7"
-          />
-        </Button>
-        <Text className="text-3xl font-medium text-green-500 mt-3">
-          OUR TEAM
-        </Text>
-        <Text className="text-sm text-[#3e3e3e] font-medium">
-          WWS has a network of consultants, industry experts, technical gurus
-          and support staff that spans the globe, and offers clients a truly
-          global perspective on a broad range of industries, markets, and
-          technologies.
-        </Text>
-      </View>
+      <LinearGradient
+        className="h-screen "
+        colors={["#6cb676", "#396ea1"]}
+        start={{ x: 0.2, y: 0.2 }}
+        end={{ x: 0.8, y: 1 }}
+      >
+        <ScrollView className="h-screen p-4 bg-transparent">
+          <Button
+            className="rounded-full w-7 bg-transparent "
+            onPress={() => navigation.goBack()}
+          >
+            <Image
+              source={require("../../assets/images/ItSolutions/arrow.png")}
+              className="w-7 h-7"
+            />
+          </Button>
+          <Text className="text-3xl font-medium text-green-200 mt-3">
+            OUR TEAM
+          </Text>
+          <Text className="text-sm text-[#3e3e3e] font-medium">
+            WWS has a network of consultants, industry experts, technical gurus
+            and support staff that spans the globe, and offers clients a truly
+            global perspective on a broad range of industries, markets, and
+            technologies.
+          </Text>
+        </ScrollView>
+      </LinearGradient>
     </NativeBaseProvider>
   );
 };
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,83 +1,91 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Settings = () => {
   return (
-    <View className="p-6 bg-white h-screen">
-      <Text className="text-4xl font-bold p-4">Settings</Text>
+    <LinearGradient
+      className="h-screen "
+      colors={["#6cb676", "#396ea1"]}
+      start={{ x: 0.2, y: 0.2 }}
+      end={{ x: 0.8, y: 1 }}
+    >
+      <View className="p-6 bg-transparent h-screen">
+        <Text className="text-4xl font-bold p-4">Settings</Text>
 
-      {/* Themes */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-6 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/themes.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Themes</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
+        {/* Themes */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-6 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/themes.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Themes</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
 
-      {/* Username */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/username.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Username</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
+        {/* Username */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/username.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Username</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
 
-      {/* Notifications */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/notification.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Notifications</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
+        {/* Notifications */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/notification.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Notifications</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
 
-      {/* Privacy */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/privacy.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Privacy</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
+        {/* Privacy */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/privacy.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Privacy</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
 
-      {/* Account */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/account.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Account</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
+        {/* Account */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/account.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Account</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
 
-      {/* Help & Support */}
-      <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
-        <View className="flex-row items-center gap-3">
-          <Image
-            source={require("../../assets/images/settings/help.png")}
-            className="h-[30px] w-[30px]"
-          />
-          <Text className="text-lg text-[#333333]">Help & Support</Text>
-        </View>
-        <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
-      </TouchableOpacity>
-    </View>
+        {/* Help & Support */}
+        <TouchableOpacity className="flex-row items-center justify-between mt-3 bg-slate-200 rounded-lg px-4 py-4 shadow-md">
+          <View className="flex-row items-center gap-3">
+            <Image
+              source={require("../../assets/images/settings/help.png")}
+              className="h-[30px] w-[30px]"
+            />
+            <Text className="text-lg text-[#333333]">Help & Support</Text>
+          </View>
+          <Text className="text-xl text-[#3e3e3e] font-medium">&gt;</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
 
